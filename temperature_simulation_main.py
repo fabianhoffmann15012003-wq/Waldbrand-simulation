c179c7423278795d2940079570bb400a8ac34367
Malin Haas <malinluisa03@gmail.com>
2025-11-17 22:52:54 +0100

    added comments and fixed small mistakes

--- temperature_simulation_main.py
+++ temperature_simulation_main.py
@@ -10,8 +10,8 @@ import time
 # Constants ? from the Table 2 
 FMC = 0.25
 H = 2
-T_A = 300
-T_MAX_I = 1200
+T_A = 300 #interpreting as outside Temperature ~25°C
+T_MAX_I = 1200 # interpreting as hottest possible wildfire ~927°C
 RHO_G = 1
 RHO_S = 700
 C_PG = 1043
@@ -42,6 +42,7 @@ KAPPA = 0.41 # Karman's Konstant
 def gauss2d(x, y, mx, my, s):
     return 1./(2.*np.pi*s*s)*np.exp(-((x-mx)**2./(2.*s**2.)+(y-my)**2./(2.*s**2.)))
 
+
 class Sim:
 
     def __init__(self, NX=100, NY=100, U_10_X=10, U_10_Y=0):
@@ -53,16 +54,19 @@ class Sim:
         #Z_0 = 0.25
         #DELTA = 0.04
 
+        # Initial Shape, Fuel and Temperature
         self.NX, self.NY = 100, 100
         self.S_1_matrix = np.ones((self.NX, self.NY))*0.2 # shape of the forest
         self.S_2_matrix = np.ones((self.NX, self.NY))*0.8 # shape of the forest
         self.S_2_0_matrix = self.S_2_matrix
         self.S_matrix = self.calc_S()
-        self.T_matrix = np.ones((self.NX,self.NY))*T_A # shape of the forest 
+        # Temperature is spread by a gaussian in the middle
+        self.T_matrix = np.ones((self.NX,self.NY))*T_A 
         for i in range(self.NX):
             for j in range(self.NY):
-                self.T_matrix[i,j] += gauss2d(i, j, self.NX//2, self.NY//2, self.NX//20)*700
+                self.T_matrix[i,j] += gauss2d(i, j, self.NX//2, self.NY//2, self.NX//20)*(T_MAX_I-T_A)
 
+        # initial Speeds
         self.U_10_X = U_10_X # wind with speed 10 m/s in only the x-direction
         self.U_10_Y = U_10_Y # wind with speed 10 m/s in only the x-direction
         self.U_H_X = self.U_10_X*0.9
@@ -75,50 +79,61 @@ class Sim:
         self.dt = 0.1 # length of time steps from paper in seconds
         self.dx = 0.5 # length of a "pixel" from paper in m
 
-        self.x_c = self.calc_x_c()
-        self.avg_u_x = self.calc_avg_u_x()
-        self.avg_u_y = 0
-        self.c_0 = self.calc_c_0()
-        self.c_1 = self.calc_c_1()
-        self.L_x, self.L_y = self.calc_L()
-        self.omega_x, self.omega_y = self.calc_omega()
-        self.D_eff_x, self.D_eff_y = self.calc_D_eff()
 
         
-    # calculations 
+    #  --- calculations 
+
+    # calculates the varible c_0 that ist dependent on the matrix S
     def calc_c_0(self):
         return ALPHA*self.S_matrix + (1-ALPHA)*LAMBDA*GAMMA + ALPHA*GAMMA*(1-self.S_matrix)
     
+    # calculates the varible c_1 that ist dependent on the matrix S and can be written as a product of c_0
     def calc_c_1(self):
         return self.c_0-ALPHA*self.S_matrix
     
+    # calculates the matrix S_1  that is dependent on the Matrix T
+    # S_1 describes the remaining fuell mass fraction of water
     def calc_S_1(self):
         r_1 = C_S1*np.exp(-B_1/self.T_matrix)
         return self.S_1_matrix-self.S_1_matrix*r_1*self.dt
-    
+        
+    # calculates the matrix S_2  that is dependent on the Matrix T and the average Speed u_avg
+    # S_1 describes the remaining fuell mass fraction of combustibles
     def calc_S_2(self):
         r_2 = C_S2*np.exp(-B_2/self.T_matrix)
         r_m = R_M_0+R_M_C*(self.avg_u_x-1)
         r_2t = (r_2*r_m)/(r_2+r_m)
         return self.S_2_matrix-self.S_2_matrix*r_2t*self.dt
-    
+        
+    # calculates the matrix S that is dependent on the Matrix S_1 and S_2
+    # S_1 describes the total fuell mass fraction
     def calc_S(self):
         return self.S_1_matrix+self.S_2_matrix
-
+    
+    # calculates the varible x_c  that is dependent on the Matrix S_2 as well as it's initial state S_2_0
     def calc_x_c(self):
         return self.S_2_matrix/self.S_2_0_matrix
     
+    # calculates the varible u_avg_x that is dependent on the varible x_c
+    # it describes the average speed over the forest an at this point is only in the x-Direction as the y-Paart =0
     def calc_avg_u_x(self):
         return self.AVG_U_V_X+(self.AVG_U_B_X-self.AVG_U_V_X)*(1-self.x_c)
 
+    # calculates the vector L that is dependent on the matrix T
+    # it describes the size of the fire in the two Dimensions
     def calc_L(self):
-        T_max = np.max(self.T_matrix)
-        T_dropped = 0.1*T_max +T_A
-        T_max_x, T_max_y = np.unravel_index(np.argmax(self.T_matrix), self.T_matrix.shape)
-        L_x = (np.sum(self.T_matrix[T_max_x,:]>T_dropped)-1)*self.dx
-        L_y = (np.sum(self.T_matrix[:,T_max_y]>T_dropped)-1)*self.dx
+        T_max = np.max(self.T_matrix) # maximum Tmeperature value
+        T_dropped = 0.1*T_max +T_A # the threshold at which it is not considered on fire anymore
+        T_max_x, T_max_y = np.unravel_index(np.argmax(self.T_matrix), self.T_matrix.shape) # the coordinates of the maximum value
+        # calculates the lenght in each direction by creating a 1D vector of the corresponding x-collum/y-line and evaluating
+        # if the temperature of the pixel has dropped below T_dropped
+        # the pixels that are above get asinged 1 while the others are 0, the sum*dx is therefore the lenght
+        L_x = (np.sum(self.T_matrix[T_max_x,:]>T_dropped))*self.dx
+        L_y = (np.sum(self.T_matrix[:,T_max_y]>T_dropped))*self.dx
         return L_x, L_y
 
+    # calculates the vector omega that is dependent on the matrix T
+    # it describes the fireline length
     def calc_omega(self):
         T_max_x = [np.argmax(self.T_matrix[i][0:]) for i in range(self.NX)]    #finding maxima in x direction
         yA = yB = xA = xB = -1      #initialize values
@@ -137,6 +152,7 @@ class Sim:
         omega_y = abs(yB-yA)*self.dx
         return omega_x, omega_y
 
+    # calculates the two values in the Matrix D_eff that are dependent on the vectors L and omega
     def calc_D_eff(self):
         D_eff_x = D_RB+A_D*self.avg_u_x*self.L_x*(1-np.exp(-GAMMA_D*self.omega_x))
         D_eff_y = D_RB+A_D*self.avg_u_y*self.L_y*(1-np.exp(-GAMMA_D*self.omega_y))
@@ -146,13 +162,16 @@ class Sim:
 
 
     # calculation of the Temperature => calculating a step
-
+    # using the differential equation in the paper
     def calc_T(self):
         T_matrix_new = np.copy(self.T_matrix)
-        dx2_kern = [[1,-2,1]]
+        # spacial derivatives using central difference
+        # df/dx -> (f(x-1)-f(x+1))/2
         dx_kern = [[1/2,0,-1/2]]
-        dy2_kern = [[1],[-2],[1]]
         dy_kern = [[1/2],[0],[-1/2]]
+        # d^2f/dx^2 -> f(x-1)-2f(x)+f(x+1)
+        dx2_kern = [[1,-2,1]]
+        dy2_kern = [[1],[-2],[1]]
         T_matrix_dx2 = convolve2d(self.T_matrix, dx2_kern, mode='same', boundary='symm')
         T_matrix_dy2 = convolve2d(self.T_matrix, dy2_kern, mode='same', boundary='symm')
         T_matrix_dx = convolve2d(self.T_matrix, dx_kern, mode='same', boundary='symm')
@@ -161,32 +180,26 @@ class Sim:
         T_matrix_new = T_matrix_new + dT_dt_matrix*self.dt
         return T_matrix_new
             
-
-
-
-
+    # calculates a step of the simulation
     def step(self, step):
-        x_c = self.calc_x_c()
-        avg_u_x = self.calc_avg_u_x()
+        self.x_c = self.calc_x_c()
+        self.avg_u_x = self.calc_avg_u_x()
+        self.avg_u_y = 0
+        self.c_0 = self.calc_c_0()
+        self.c_1 = self.calc_c_1()
+        self.L_x, self.L_y = self.calc_L()
+        self.omega_x, self.omega_y = self.calc_omega()
+        self.D_eff_x, self.D_eff_y = self.calc_D_eff()
+
         S_1_matrix_new = self.calc_S_1()
         S_2_matrix_new = self.calc_S_2()
         S_matrix_new = self.calc_S()
-        c_0 = self.calc_c_0()
-        c_1 = self.calc_c_1()
-        L_x, L_y = self.calc_L()
-        omega_x, omega_y = self.calc_omega()
-        omega_x, omega_y = self.calc_D_eff()
+
         self.T_matrix = self.calc_T()
-        self.x_c = x_c
-        self.avg_u_x = avg_u_x
+
         self.S_1_matrix = S_1_matrix_new
         self.S_2_matrix = S_2_matrix_new
         self.S_matrix = S_matrix_new
-        self.c_0 = c_0
-        self.c_1 = c_1
-        self.L_x, self.L_y = L_x, L_y
-        self.omega_x, self.omega_y = omega_x, omega_y
-        self.D_eff_x, self.D_eff_y = omega_x, omega_y
 
 
 
@@ -198,10 +211,11 @@ def update(frame):
 
 
 
+# handles the animation and is also coppied from the rudimentary pixel simulation
 start = time.time()
 
 simualtion = Sim()
-frms = 1000
+frms = 500
 
 fig, ax = plt.subplots(figsize=(16,16))
 im = ax.imshow(simualtion.T_matrix, vmin=T_A)
