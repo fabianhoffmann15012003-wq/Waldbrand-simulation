541eb366d0af7abe82fe9145abcf3fe3823897eb
Malin Haas <malinluisa03@gmail.com>
2025-11-17 01:25:11 +0100

    simulation läuft, es braucht noch ebsser startbedingungn zu testen obs funktioniert

--- temperature_simulation_main.py
+++ temperature_simulation_main.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.signal import convolve2d
+import matplotlib.pyplot as plt
+from matplotlib import animation
 
 # Constants ? from the Table 2 
 FMC = 0.25
@@ -33,127 +35,167 @@ C_3 = ALPHA*A_2/C_PS
 C_4 = 1/(H*RHO_S*C_PS)
 KAPPA = 0.41 # Karman's Konstant
 
-# calculations 
+class Sim:
 
-def calc_c_0(S):
-    return ALPHA*S + (1-ALPHA)*LAMBDA*GAMMA + ALPHA*GAMMA*(1-S)
+    def __init__(self, NX=100, NY=100, U_10_X=5, U_10_Y=0):
+        # --- Initial conditions
+        # Sparse Canopy
+        Z_0 = 0.5
+        DELTA = 0.08
+        # Dense Canopy
+        #Z_0 = 0.25
+        #DELTA = 0.04
 
-def calc_c_1(S, c_0):
-    return c_0-ALPHA*S
+        self.NX, self.NY = 100, 100
+        self.S_1_matrix = np.ones((self.NX, self.NY))*0.8 # shape of the forest
+        self.S_2_matrix = np.ones((self.NX, self.NY))*0.8 # shape of the forest
+        self.S_2_0_matrix = self.S_2_matrix
+        self.S_matrix = self.calc_S()
+        self.T_matrix = np.ones((self.NX,self.NY))*T_A # shape of the forest 
 
-def calc_S_1(S1_alt_matrix, T_matrix, dt):
-    r_1 = C_S1*np.exp(-B_1/T_matrix)
-    return S1_alt_matrix-S1_alt_matrix*r_1*dt
+        self.U_10_X = U_10_X # wind with speed 10 m/s in only the x-direction
+        self.U_10_Y = U_10_Y # wind with speed 10 m/s in only the x-direction
+        self.U_H_X = self.U_10_X*0.9
+        self.U_B_STAR_X = self.U_10_X*KAPPA/np.log(10/Z_0)
+        self.AVG_U_V_X = self.U_H_X/ETA * (1-np.exp(-ETA))
+        self.AVG_U_B_X = self.U_B_STAR_X/KAPPA * (H/(H-Z_0)*np.log(H/Z_0)-1)
 
-def calc_S_2(S2_alt_matrix, T_matrix, avg_u_x, dt):
-    r_2 = C_S2*np.exp(-B_2/T_matrix)
-    r_m = R_M_0+R_M_C*(avg_u_x-1)
-    r_2t = (r_2*r_m)/(r_2+r_m)
-    return S2_alt_matrix-S2_alt_matrix*r_2t*dt
+        # --- simulation conditions
+        self.N = 1000 # number of steps
+        self.dt = 0.1 # length of time steps from paper in seconds
+        self.dx = 0.5 # length of a "pixel" from paper in m
 
-def calc_S(S_1, S_2):
-    return S_1+S_2
+        self.x_c = self.calc_x_c()
+        self.avg_u_x = self.calc_avg_u_x()
+        self.avg_u_y = 0
+        self.c_0 = self.calc_c_0()
+        self.c_1 = self.calc_c_1()
+        self.L_x, self.L_y = self.calc_L()
+        self.omega_x, self.omega_y = self.calc_omega()
+        self.D_eff_x, self.D_eff_y = self.calc_D_eff()
 
-def calc_x_c(S_2_matrix, S_2_0_matrix):
-    return S_2_matrix/S_2_0_matrix
+        
+    # calculations 
+    def calc_c_0(self):
+        return ALPHA*self.S_matrix + (1-ALPHA)*LAMBDA*GAMMA + ALPHA*GAMMA*(1-self.S_matrix)
+    
+    def calc_c_1(self):
+        return self.c_0-ALPHA*self.S_matrix
+    
+    def calc_S_1(self):
+        r_1 = C_S1*np.exp(-B_1/self.T_matrix)
+        return self.S_1_matrix-self.S_1_matrix*r_1*self.dt
+    
+    def calc_S_2(self):
+        r_2 = C_S2*np.exp(-B_2/self.T_matrix)
+        r_m = R_M_0+R_M_C*(self.avg_u_x-1)
+        r_2t = (r_2*r_m)/(r_2+r_m)
+        return self.S_2_matrix-self.S_2_matrix*r_2t*self.dt
+    
+    def calc_S(self):
+        return self.S_1_matrix+self.S_2_matrix
 
+    def calc_x_c(self):
+        return self.S_2_matrix/self.S_2_0_matrix
+    
+    def calc_avg_u_x(self):
+        return self.AVG_U_V_X+(self.AVG_U_B_X-self.AVG_U_V_X)*(1-self.x_c)
 
+    def calc_L(self):
+        T_max = np.max(self.T_matrix)
+        T_dropped = 0.1*T_max +T_A
+        T_max_x, T_max_y = np.unravel_index(np.argmax(self.T_matrix), self.T_matrix.shape)
+        L_x = (np.sum(self.T_matrix[T_max_x,:]>T_dropped)-1)*self.dx
+        L_y = (np.sum(self.T_matrix[:,T_max_y]>T_dropped)-1)*self.dx
+        return L_x, L_y
 
-def calc_avg_u_x(x_c):
-    avg_u_x = AVG_U_V_X+(AVG_U_B_X-AVG_U_V_X)*(1-x_c)
-    return avg_u_x
-
-def calc_L(T_matrix):
-    T_max = np.max(T_matrix)
-    T_dropped = 0.1*T_max +T_A
-    T_max_x, T_max_y = np.unravel_index(np.argmax(T_matrix), T_matrix.shape)
-    L_x = (np.sum(T_matrix[T_max_x,:]>T_dropped)-1)*dx
-    L_y = (np.sum(T_matrix[:,T_max_y]>T_dropped)-1)*dx
-    return L_x, L_y
-
-def calc_omega(T_matrix, NX, NY):
-    T_max_x = [np.argmax(T_matrix[i][0:]) for i in range(NX)]    #finding maxima in x direction
-    yA = yB = xA = xB = -1      #initialize values
-    for i in range(NX):     #finding endpoints xA, xB, yA, yB by condition
-        if yA < 0 and T_matrix[i][T_max_x[i]] > 550: #only once at beginning for y
-            yA = i
-            xA = T_max_x[i]
-        if T_matrix[i][T_max_x[i]] > 550:   #reset values until done
-            yB = i
-            if T_max_x[i] >= xB:    #check if new xB is actually larger than old one
-                xB = T_max_x[i]
-            if T_max_x[i] < xA:
+    def calc_omega(self):
+        T_max_x = [np.argmax(self.T_matrix[i][0:]) for i in range(self.NX)]    #finding maxima in x direction
+        yA = yB = xA = xB = -1      #initialize values
+        for i in range(self.NX):     #finding endpoints xA, xB, yA, yB by condition
+            if yA < 0 and self.T_matrix[i][T_max_x[i]] > 550: #only once at beginning for y
+                yA = i
                 xA = T_max_x[i]
+            if self.T_matrix[i][T_max_x[i]] > 550:   #reset values until done
+                yB = i
+                if T_max_x[i] >= xB:    #check if new xB is actually larger than old one
+                    xB = T_max_x[i]
+                if T_max_x[i] < xA:
+                    xA = T_max_x[i]
 
-    omega_x = abs(xB-xA)*dx     #computing the difference between endpoints
-    omega_y = abs(yB-yA)*dx
-    return omega_x, omega_y
+        omega_x = abs(xB-xA)*self.dx     #computing the difference between endpoints
+        omega_y = abs(yB-yA)*self.dx
+        return omega_x, omega_y
 
-def calc_D_eff(L_x, L_y, omega_x, omega_y, avg_u_x, avg_u_y=0):
-    D_eff_x = D_RB+A_D*avg_u_x*L_x*(1-np.exp(-GAMMA_D*omega_x))
-    D_eff_y = D_RB+A_D*avg_u_y*L_y*(1-np.exp(-GAMMA_D*omega_y))
-    return D_eff_x, D_eff_y
-
-
+    def calc_D_eff(self):
+        D_eff_x = D_RB+A_D*self.avg_u_x*self.L_x*(1-np.exp(-GAMMA_D*self.omega_x))
+        D_eff_y = D_RB+A_D*self.avg_u_y*self.L_y*(1-np.exp(-GAMMA_D*self.omega_y))
+        return D_eff_x, D_eff_y
 
 
-# calculation of the Temperature => calculating a step
 
-def calc_T(T_matrix, c_0, c_1, D_eff_x, D_eff_y, avg_u_x, dt, avg_u_y=0):
-    T_matrix_new = np.copy(T_matrix)
-    dy2_kern = [[1,-2,1]]
-    dy_kern = [[-1/2,0,1/2]]
-    dx2_kern = [[1],[-2],[1]]
-    dx_kern = [[-1/2],[0],[1/2]]
-    T_matrix_dx2 = convolve2d(T_matrix, dx2_kern, mode='same', boundary='symm')
-    T_matrix_dy2 = convolve2d(T_matrix, dy2_kern, mode='same', boundary='symm')
-    T_matrix_dx = convolve2d(T_matrix, dx_kern, mode='same', boundary='symm')
-    T_matrix_dy = convolve2d(T_matrix, dy_kern, mode='same', boundary='symm')
-    dT_dt_matrix = c_1/c_0*(D_eff_x*T_matrix_dx2 + D_eff_y*T_matrix_dy2-avg_u_x*T_matrix_dx-avg_u_y*T_matrix_dy)
-    T_matrix_new = T_matrix_new + dT_dt_matrix*dt
-    return T_matrix
+
+    # calculation of the Temperature => calculating a step
+
+    def calc_T(self):
+        T_matrix_new = np.copy(self.T_matrix)
+        dy2_kern = [[1,-2,1]]
+        dy_kern = [[-1/2,0,1/2]]
+        dx2_kern = [[1],[-2],[1]]
+        dx_kern = [[-1/2],[0],[1/2]]
+        T_matrix_dx2 = convolve2d(self.T_matrix, dx2_kern, mode='same', boundary='symm')
+        T_matrix_dy2 = convolve2d(self.T_matrix, dy2_kern, mode='same', boundary='symm')
+        T_matrix_dx = convolve2d(self.T_matrix, dx_kern, mode='same', boundary='symm')
+        T_matrix_dy = convolve2d(self.T_matrix, dy_kern, mode='same', boundary='symm')
+        dT_dt_matrix = self.c_1/self.c_0*(self.D_eff_x*T_matrix_dx2 + self.D_eff_y*T_matrix_dy2-self.avg_u_x*T_matrix_dx-self.avg_u_y*T_matrix_dy)
+        T_matrix_new = T_matrix_new + dT_dt_matrix*self.dt
+        return T_matrix_new
             
 
 
 
-# --- Initial conditions
-# Sparse Canopy
-Z_0 = 0.5
-DELTA = 0.08
-# Dense Canopy
-#Z_0 = 0.25
-#DELTA = 0.04
 
-NX, NY = 100, 100
-S_1_matrix = np.ones((NX, NY))*0.8 # shape of the forest
-S_2_matrix = np.ones((NX, NY))*0.8 # shape of the forest
-S_2_0_matrix = S_2_matrix
-T_matrix = np.ones((NX,NY)) # shape of the forest 
-
-U_10_X = 5 # wind with speed 10 m/s in only the x-direction
-U_H_X = U_10_X*0.9
-U_B_STAR_X = U_10_X*KAPPA/np.log(10/Z_0)
-AVG_U_V_X = U_H_X/ETA * (1-np.exp(-ETA))
-AVG_U_B_X = U_B_STAR_X/KAPPA * (H/(H-Z_0)*np.log(H/Z_0)-1)
-
-# --- simulation conditions
-N = 1000 # number of steps
-dt = 0.1 # length of time steps from paper in seconds
-dx = 0.5 # length of a "pixel" from paper in m
-
-def step():
-    x_c = calc_x_c(S_2_matrix, S_2_0_matrix)
-    avg_u_x = calc_avg_u_x(x_c)
-    S_1_matrix_new = calc_S_1(S_1_matrix, T_matrix, dt)
-    S_2_matrix_new = calc_S_2(S_2_matrix, T_matrix, avg_u_x, dt)
-    S_matrix_new = calc_S(S_1_matrix_new, S_2_matrix_new)
-    c_0 = calc_c_0(S_matrix_new)
-    c_1 = calc_c_1(S_matrix_new, c_0)
-    L_x, L_y = calc_L(T_matrix)
-    omega_x, omega_y = calc_omega(T_matrix, NX, NY)
-    D_eff_x, D_eff_y = calc_D_eff(L_x, L_y, omega_x, omega_y, avg_u_x)
-    T_new = calc_T(T_matrix, c_0, c_1, D_eff_x, D_eff_y, avg_u_x, dt)
+    def step(self, step):
+        x_c = self.calc_x_c()
+        avg_u_x = self.calc_avg_u_x()
+        S_1_matrix_new = self.calc_S_1()
+        S_2_matrix_new = self.calc_S_2()
+        S_matrix_new = self.calc_S()
+        c_0 = self.calc_c_0()
+        c_1 = self.calc_c_1()
+        L_x, L_y = self.calc_L()
+        omega_x, omega_y = self.calc_omega()
+        omega_x, omega_y = self.calc_D_eff()
+        self.T_matrix = self.calc_T()
+        self.x_c = x_c
+        self.avg_u_x = avg_u_x
+        self.S_1_matrix = S_1_matrix_new
+        self.S_2_matrix = S_2_matrix_new
+        self.S_matrix = S_matrix_new
+        self.c_0 = c_0
+        self.c_1 = c_1
+        self.L_x, self.L_y = L_x, L_y
+        self.omega_x, self.omega_y = omega_x, omega_y
+        self.D_eff_x, self.D_eff_y = omega_x, omega_y
 
 
 
-print("DONE")
+def update(frame):
+    im.set_data(simualtion.T_matrix)
+    ax.axis('off')
+    simualtion.step(frame)
+    return [im]
+
+
+simualtion = Sim()
+
+fig, ax = plt.subplots(figsize=(16,16))
+im = ax.imshow(simualtion.T_matrix)
+plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, 
+            hspace = 0, wspace = 0)
+
+ani = animation.FuncAnimation(fig, update, frames=100, interval=20) #frames - the number of steps in the simulation
+ani.save('Animations/simple.gif', fps=1.5, savefig_kwargs={'pad_inches':0})
+
+print("\n------------------------------ ! ! ! FERTIG ! ! ! ------------------------------\n")
+
